3d23cedc1b0de6543069e36fb4d071d1ff814817
Karpuk Vladimir <132531vova@gmail.com>
2022-05-13 23:55:00 +0300

    Modified main.c for the intersection

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,6 +1,4 @@
 #include <libgeometry/functions.h>
-#include <stdio.h>
-#include <string.h>
 
 int main()
 {
@@ -8,19 +6,26 @@ int main()
 
     int lines = check_line();
 
+    circle circles[lines];
+
+    int circles_count;
+
     FILE* file = fopen("input.txt", "r");
     for (int line = 0; line < lines; line++) {
         fgets(str, 100, file);
         str[strlen(str) - 1] = '\0';
 
         if (isCorrect(str)) {
-            circle tr1;
-            tr1 = getcircle(str);
-            writePandS(tr1);
+            circles[circles_count++] = getcircle(str);
         }
-        printf("\n");
     }
 
     fclose(file);
+
+        for (int i = 0; i < circles_count; i++)
+        writePandS(circles[i]);
+
+    PrintIntersect(circles, circles_count);
+
     return 0;
 }
