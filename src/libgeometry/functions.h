13aa5a818f8f0e962252442d4f63eaf9e5ca6644
Karpuk Vladimir <132531vova@gmail.com>
2022-05-13 11:13:34 +0300

    update libs

---
+++ src/libgeometry/functions.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+typedef struct {
+    double x;
+    double y;
+    double r;
+} circle;
+
+int check_line();
+int isCorrect(char* str);
+circle getcircle(char* str);
+void writePandS(circle tr1);
+
+#endif //  _FUNCTIONS_H_
