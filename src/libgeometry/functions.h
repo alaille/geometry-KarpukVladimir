747397ac74b484dc0e65a962e076e6eb1f44e559
Karpuk Vladimir <132531vova@gmail.com>
2022-05-13 23:56:37 +0300

    Modified lib

--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -14,8 +14,9 @@ typedef struct {
 } circle;
 
 int check_line();
-int isCorrect(char* str);
-circle getcircle(char* str);
-void writePandS(circle tr1);
+int isCorrect(char* );
+circle getcircle(char* );
+void writePandS(circle );
+void PrintIntersect(circle *, int);
 
 #endif //  _FUNCTIONS_H_
